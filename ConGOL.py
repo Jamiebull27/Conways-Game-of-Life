eb609a4e531d11206b71df9a607e1fac7a4c3749
Jamie Bull <33397912+Jamiebull27@users.noreply.github.com>
2017-11-09 02:17:37 +0000

    New was to initialise seeds
    Changed initialisation of seeds from pre-built seeds to .lif files which
    can be found on conwaylife.com/wiki.

--- ConGOL.py
+++ ConGOL.py
@@ -33,105 +33,38 @@ def updateCell(i, j):
 		else:
 			return 0
 	
-#create pulsar seed (requires 15x15 grid)#
-def seedPulsar():
+#Read seed specified by user to initialise cells#
+def readSeed():
+
+	print("Enter the file name of the seed you would like to use:\n"
+	      "(expecting format of .lif 1.06 which can be found on conwaylife.com/wiki)\n")
 	
-	grid[3][1] = 1
-	grid[4][1] = 1
-	grid[5][1] = 1
-	grid[9][1] = 1
-	grid[10][1] = 1
-	grid[11][1] = 1
-	grid[1][3] = 1
-	grid[6][3] = 1
-	grid[8][3] = 1
-	grid[13][3] = 1
-	grid[1][4] = 1
-	grid[6][4] = 1
-	grid[8][4] = 1
-	grid[13][4] = 1
-	grid[1][5] = 1
-	grid[6][5] = 1
-	grid[8][5] = 1
-	grid[13][5] = 1
-	grid[3][6] = 1
-	grid[4][6] = 1
-	grid[5][6] = 1
-	grid[9][6] = 1
-	grid[10][6] = 1
-	grid[11][6] = 1
-	grid[3][8] = 1
-	grid[4][8] = 1
-	grid[5][8] = 1
-	grid[9][8] = 1
-	grid[10][8] = 1
-	grid[11][8] = 1
-	grid[1][9] = 1
-	grid[6][9] = 1
-	grid[8][9] = 1
-	grid[13][9] = 1
-	grid[1][10] = 1
-	grid[6][10] = 1
-	grid[8][10] = 1
-	grid[13][10] = 1
-	grid[1][11] = 1
-	grid[6][11] = 1
-	grid[8][11] = 1
-	grid[13][11] = 1
-	grid[3][13] = 1
-	grid[4][13] = 1
-	grid[5][13] = 1
-	grid[9][13] = 1
-	grid[10][13] = 1
-	grid[11][13] = 1
+	exit = 0
 	
-#create blinker seed (requires 5x5 grid)#
-def seedBlinker():
-	
-	grid[2][1] = 1
-	grid[2][2] = 1
-	grid[2][3] = 1
-	
-#create Gosper glider gun seed (requires 50x50 grid) WIP#
-def seedGG():
-	
-	grid[1][42] = 1
-	grid[1][43] = 1
-	grid[2][44] = 1
-	grid[2][43] = 1
-	grid[11][41] = 1
-	grid[11][42] = 1
-	grid[11][43] = 1
-	grid[12][40] = 1
-	grid[12][44] = 1
-	grid[13][39] = 1
-	grid[13][45] = 1
-	grid[14][39] = 1
-	grid[14][45] = 1
-	grid[15][42] = 1
-	grid[16][40] = 1
-	grid[16][44] = 1
-	grid[17][41] = 1
-	grid[17][42] = 1
-	grid[17][43] = 1
-	grid[18][42] = 1
-	grid[21][43] = 1
-	grid[21][44] = 1
-	grid[21][45] = 1
-	grid[22][43] = 1
-	grid[22][44] = 1
-	grid[22][45] = 1
-	grid[23][42] = 1
-	grid[23][46] = 1
-	grid[25][41] = 1
-	grid[25][42] = 1
-	grid[25][46] = 1
-	grid[25][47] = 1
-	grid[35][44] = 1
-	grid[35][45] = 1
-	grid[36][44] = 1
-	grid[36][45] = 1
-	
+	while 1:
+		try:
+			filename = input()
+			if filename == "exit":	#lets user exit program if problem with filename
+				exit = 1	#Cannot exit in try
+				break
+			
+			f = open(filename, "r")
+			break
+			
+		except:
+			print("Error: invalid filename. Try again:\n(Type \"exit\" to leave the program)")
+	if exit == 1:
+		exit()
+		
+	for line in f:
+		if( line[0] != '#' ):	#check for comments
+			i,j = line.split()
+			i = int(i) + (gridSize / 2)
+			j = int(j) + (gridSize / 2)
+			grid[int(i)][int(j)] = 1	#Make sure int as can be float depending on gridsize
+		
+	f.close()
+		
 #Updates all cells at once#
 def updateAll():
 	
@@ -146,8 +79,9 @@ def updateAll():
 			grid[i][j] = newGrid[i][j]
 
 #global#
-grid = np.zeros((50,50))	#All cells initialised as dead (0) (alive = 1)
-seedGG()
+gridSize = 50	#50 should be enough for most simple seeds
+grid = np.zeros((gridSize, gridSize))	#All cells initialised as dead (0) (alive = 1)
+readSeed()	#Initialisation seed
 
 fig = plt.figure()
 ax = plt.axes(xlim = (-1, grid.shape[0]), ylim = (-1, grid.shape[1]))
@@ -173,7 +107,7 @@ def animate(i):
 	updateAll()
 	return cells,
 
-anim = animation.FuncAnimation(fig, animate, init_func=init, frames=1000, interval=25, blit=True)
+anim = animation.FuncAnimation(fig, animate, init_func=init, frames=200, interval=25, blit=True)
 
 plt.grid()
 plt.show()
